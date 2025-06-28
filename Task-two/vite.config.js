15346489486c7462a8eee8be7f0999b5e98959f1
veeresh-h <veereshhedderi18@gmail.com>
2025-06-28 20:03:09 +0530

    Added new features to project

--- Task-two/vite.config.js
+++ Task-two/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  base: '/MainflowService/', // 👈 very important for GitHub Pages
-  plugins: [react(), tsconfigPaths()],
+  base: "/MainflowService/", // ✅ your repo name
+  plugins: [
+    react(),
+    tsconfigPaths(),          // ✅ enables @ alias from jsconfig.json
+  ],
 })
